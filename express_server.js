50ce94724cfc5f0a6bfe3ad5881ca7893de52050
Aidan <your@email.com>
2019-07-09 21:29:40 +0000

    Added in delete button

--- express_server.js
+++ express_server.js
@@ -11,7 +11,7 @@ const generateRandomString = function() {
   return result;
 };
 
-console.log(generateRandomString());
+//console.log(generateRandomString());
 
 app.set('view engine', 'ejs');
 
@@ -37,7 +37,7 @@ app.get("/hello", (req, res) => {
 });
 
 
-//RENDERING URLs
+//Rendering URLs
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -52,15 +52,17 @@ app.get("/urls", (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+//Redirect to longURL 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);  
 });
 
-app.post("/urls", (req, res) => {
-  urlDatabase[`${generateRandomString()}`] = req.body.longURL;
-  console.log(req.body);  // Log the POST request body to the console
-  res.redirect('/urls');        
+
+//Delete 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];   
+  res.redirect('/urls');
 });
 
 
