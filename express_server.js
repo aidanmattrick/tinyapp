d37444c992b2d3b79802b22e94d428560e3fc835
Aidan <your@email.com>
2019-07-09 01:17:17 +0000

    added in short URL redirect

--- express_server.js
+++ express_server.js
@@ -52,9 +52,15 @@ app.get("/urls", (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);  
+});
+
 app.post("/urls", (req, res) => {
+  urlDatabase[`${generateRandomString()}`] = req.body.longURL;
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  res.redirect('/urls');        
 });
 
 
