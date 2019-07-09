99868f939f60e685a9396184d9940af1b5d3bf68
Aidan <your@email.com>
2019-07-09 00:34:59 +0000

    allows user to input a longURL and send that data to our API via a POST request

--- express_server.js
+++ express_server.js
@@ -2,6 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const generateRandomString = function() {
+  let result = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
+
+console.log(generateRandomString());
+
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
@@ -9,6 +20,8 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 //TESTING CODE
 app.get("/", (req, res) => {
@@ -25,14 +38,23 @@ app.get("/hello", (req, res) => {
 
 
 //RENDERING URLs
-app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render('urls_index', templateVars);
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get('/urls/:shortURL', (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render('urls_show', templateVars);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 
